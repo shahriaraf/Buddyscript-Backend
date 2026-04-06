20e46e97b982772ff30345d80aabc11a2d4985bc
shahriaraf <shahriaraf01@gmail.com>
2026-04-06 17:02:17 +0600

    fixed deployment

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log'],
